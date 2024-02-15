a4ff08a26f37f21b16bd1cf21e6c98670e8c5834
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-02-15 14:19:08 -0300

    feat: create router.ts

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { router } from './router';
 
 const app = express();
 const port = process.env.PORT || 3003;
@@ -12,4 +13,5 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 
+app.use(router);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
