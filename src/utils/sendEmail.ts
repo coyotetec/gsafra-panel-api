6ec041c2683b837c6676da5d9e3a9b4eb099d695
raul.souza <raulcesar54@hotmail.com>
2025-03-17 17:29:30 -0400

    feat

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -14,6 +14,7 @@ export async function sendEmail(
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
